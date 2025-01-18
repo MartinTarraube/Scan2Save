737dd3fc05310817b48df943deba1182fb88eba9
Zachary Chuang <zmchuang@gmail.com>
2025-01-18 16:12:20 -0500

    + martins changes

--- app.py
+++ app.py
@@ -2,6 +2,9 @@ from flask import Flask, render_template, request, redirect, url_for
 from google.cloud import vision
 import os
 import google.generativeai as genai
+import pandas as pd
+
+excel_file_path = 0
 
 app = Flask(__name__)
 os.environ['GOOGLE_APPLICATION_CREDENTIALS'] = '/home/zmchuang/McWics-hackathon/receipt-parser-448216-ad317ae49a0a.json'
@@ -11,11 +14,18 @@ def home():
 
 @app.route('/upload', methods=['GET', 'POST'])
 def upload():
+    
+
     if request.method == 'POST':
         if 'file' not in request.files: 
             raise Exception("No file part")
+
         file = request.files['file']
-        if file:
+
+        if excel_file_path == 0:
+            detect_excel(file)
+        
+        elif file:
             if file.content_type.startswith('image/'):
                 file_path = os.path.join('uploads', file.filename)
                 file.save(file_path)
@@ -29,11 +39,11 @@ def upload():
                 merchant = merchant.strip('"')
                 print(price)
                 print(date)
-                print(merchant)
+                print(merchant)     
                 
                 return render_template('result.html', text=parsed_data)
             else:
-                flash('Invalid file type. Please upload an image.')
+                raise Exception("File is of wrong type")
                 return redirect(url_for('upload'))
     return render_template('upload.html', text=None)
 
@@ -62,6 +72,38 @@ def parse_with_gemini(text):
         'Put the merchant value in quotes.')
     response = model.generate_content(prompt + text)
     return response.text
+
+def modify_excel_price(prix, the_path):
+    df = pd.read_excel(the_path)
+
+    #add new value to end of 2nd column
+    df.loc[len(df), df.columns[1]] = prix
+    total_sum = df.iloc[:, 1].sum()
+    df.to_excel(the_path, index=False)
+    return total_sum
+
+def modify_excel_date(date, the_path):
+    df = pd.read_excel(the_path)
+
+    #add new value to end of 2nd column
+    df.loc[len(df), df.columns[0]] = date
+    
+    df.to_excel(the_path, index=False)
+    return 
+
+def modify_excel_merchant(merchant, the_path):
+    df = pd.read_excel(the_path)
+
+    #add new value to end of 2nd column
+    df.loc[len(df), df.columns[2]] = merchant
+    
+    df.to_excel(the_path, index=False)
+    return
+
+def detect_excel(file):
+    if file and file.filename.endswith(('.xlsx', '.xls')):
+        excel_file_path = file
+    return
 
 if __name__ == '__main__':
     app.run(debug=True, port=5000)
